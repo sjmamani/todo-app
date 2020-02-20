af3a2c98f0c8bddd963c088f6ebc5ef5954e8e81
sjmamani <santiagojmamani@gmail.com>
2020-02-20 15:18:42 -0300

    Update todos-list.component.ts

--- src/app/todo/todos-list/todos-list.component.ts
+++ src/app/todo/todos-list/todos-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
 import { Todo } from '../models/todo.model';
+import { filtrosValidos } from '../../filter/filter.actions';
 
 @Component({
   selector: 'app-todos-list',
@@ -11,7 +12,7 @@ import { Todo } from '../models/todo.model';
 export class TodosListComponent implements OnInit {
 
   todos: Todo[] = [];
-  filtroActual: string;
+  filtroActual: filtrosValidos;
 
   constructor(private store: Store<AppState>) { }
 
